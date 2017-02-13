d83f0208851e1e409ee31c9af10cc938dfeb4067
Daniel Gempesaw <gempesaw@gmail.com>
2017-02-13 17:53:28 -0500

    Generalize restart for api boxes

--- test/restart_test.js
+++ test/restart_test.js
@@ -2,25 +2,43 @@ import { expect } from 'chai';
 import td from 'testdouble';
 import http from '~/lib/http';
 import config from '~/lib/config';
+import context from '~/lib/context';
 
 import restart from '~/lib/restart';
 
 describe('Restart', () => {
     config.debug = false;
 
-    const context = 'test-context';
+    const testContext = context.getContexts()[0];
     let getFromAdminUi;
     beforeEach(() => {
-
         getFromAdminUi = td.replace(http, 'getFromAdminUi');
     });
 
     it('should restart a server', () => {
-        td.when(getFromAdminUi(restart.uri(context, 'boxIds')))
+        td.when(getFromAdminUi(restart.uri(testContext)))
             .thenReturn(Promise.resolve('Request queued'));
 
-        const restarted = restart.queue(context, 'boxIds');
-        expect(restarted).to.be.truthy;
+        return restart.queue(testContext)
+            .then(res => {
+                expect(res).to.equal('Request queued');
+            });
+    });
+
+    it('should construct appropriate site restart urls', () => {
+        config.siteBox = 'site-box';
+        config.siteEnv = 'site-env';
+        const uri = restart.uri('test-microsite');
+        expect(uri).to.include('service=sc-site-env-test-microsite');
+        expect(uri).to.include('ids=site-box');
+    });
+
+    it('should construct appropriate site restart urls', () => {
+        config.apiBox = 'api-box';
+        config.apiEnv = 'api-env';
+        const uri = restart.uri('test-api');
+        expect(uri).to.include('service=sc-api-env-test-api');
+        expect(uri).to.include('ids=api-box');
     });
 
     afterEach(() => td.reset());
