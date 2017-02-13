f3b88d8fa64d942f8b62a30a094cb1e247a675f3
Daniel Gempesaw <gempesaw@gmail.com>
2017-02-13 17:51:44 -0500

    Generalize context lookups and coercion

--- test/context_test.js
+++ test/context_test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 
 import context from '~/lib/context';
+import config from '~/lib/config.js';
 
 describe('Context', () => {
     const fn = (context) => context;
@@ -13,6 +14,21 @@ describe('Context', () => {
 
     it('should error for invalid contexts', () => {
         expect(coerced('failure')).to.be.an('error');
+    });
 
+    it('should return api details for apis', () => {
+        const env = 'API-ENV';
+        const box = 'API-BOX';
+
+        const details = context.getDetails('whee-api');
+        expect(details).to.eql({ env, box, context: 'whee-api' });
+    });
+
+    it('should construct site details for sites', () => {
+        const env = 'SITE-ENV';
+        const box = 'SITE-BOX';
+
+        const details = context.getDetails('whee-microsite');
+        expect(details).to.eql({ env, box, context: 'whee-microsite' });
     });
 });
